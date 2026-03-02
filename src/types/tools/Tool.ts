a44398a7864d9ae8b055335c26fd253c79d8fea6
AIAS Executor <aias-executor@example.com>
2026-03-02 23:24:23 +0800

    1

--- src/types/tools/Tool.ts
+++ src/types/tools/Tool.ts
@@ -1,6 +1,7 @@
 import { ToolDefinition } from "./ToolDefinition";
 
 export interface Tool {
+    groupName?: string,
     definition: ToolDefinition;
     execute: (parameters: Record<string, any>) => Promise<any>;
 }
